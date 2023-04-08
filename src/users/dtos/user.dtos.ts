c17adfd5af3df43904edde1c3755c4132deee7b2
Iury Franca <iurygfranca@gmail.com>
2023-04-07 21:20:18 -0400

    feat: add validations for inputs ✨

--- src/users/dtos/user.dtos.ts
+++ src/users/dtos/user.dtos.ts
@@ -1,21 +1,54 @@
+// import { CpfValidator } from './../validations/cpf.validation';
+import {
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common/exceptions';
 import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  Length,
+  ValidationArguments,
+  Validate,
+} from 'class-validator';
 
 export class UserDto {
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+  @Length(3, 20, { message: 'Nome precisa ter entre 3 e 20 caracteres' })
   @ApiProperty()
   name: string;
 
+  @IsNotEmpty({ message: 'Afiliação não pode ser vazio' })
   @ApiProperty()
   affiliation: string;
 
+  @IsNotEmpty({ message: 'Data de Nascimento não pode ser vazio' })
   @ApiProperty()
   dateOfBirth: Date;
 
+  @IsNotEmpty({ message: 'CPF não pode ser vazio' })
+  // @Validate(CpfValidator)
   @ApiProperty()
   cpf: string;
 
+  @IsNotEmpty({ message: 'Email não pode ser vazio' })
+  @IsEmail()
   @ApiProperty()
   email: string;
 
+  @IsNotEmpty({ message: 'Telefone não pode ser vazio' })
+  @IsPhoneNumber('BR', {
+    message: (args: ValidationArguments) => {
+      if (args.value.length !== 10) {
+        throw new BadRequestException(
+          `${args.value} não é um número de telefone válido`,
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    },
+  })
   @ApiProperty()
   phone: string;
 }
---
+++ src/users/validations/cpf.validation.ts
@@ -0,0 +1,16 @@
+// import {
+//   ValidatorConstraint,
+//   ValidatorConstraintInterface,
+// } from 'class-validator';
+// import { cpf } from 'cpf-cnpj-validator';
+
+// @ValidatorConstraint({ name: 'CpfValidator', async: false })
+// export class CpfValidator implements ValidatorConstraintInterface {
+//   validate(numberCpf: string) {
+//     return cpf.isValid(numberCpf);
+//   }
+
+//   defaultMessage() {
+//     return 'CPF inválido, digite um que faça sentido! 😅';
+//   }
+// }
